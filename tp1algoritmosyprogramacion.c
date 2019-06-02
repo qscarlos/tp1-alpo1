3a74e1b690dc56ed8738f1d437ed46d15000d23a
qscarlos <39705706+qscarlos@users.noreply.github.com>
2019-06-02 17:15:04 -0300

    Update tp1algoritmosyprogramacion.c

--- tp1algoritmosyprogramacion.c
+++ tp1algoritmosyprogramacion.c
@@ -3,10 +3,12 @@
 #include <stdlib.h>
 #define MF 500
 #define MAX 50
-//
-//typedef enum {false, true} bool;
-typedef enum  {argentino = 1, uruguayo, chileno, peruano, boliviano, paraguayo, brasilenio}tnacion;
-typedef int tvecnaciones [7];
+#define MAxNAC 8
+
+typedef enum {false, true} bool;
+typedef enum  {argentina = 1, uruguaya, chilena, peruana, boliviana, paraguaya, brasilenia}naciones;
+typedef int tvecnaciones [MAxNAC];
+
 //typedef tnacion vecnacion[7];
 typedef struct{
 	
@@ -26,50 +28,125 @@ typedef struct{
 	
 }templeado;
 
-typedef templeado tvecempleado[MF];
+typedef templeado tvecempleados[MF];
 
-void cargar_datos (int *ml, tvecempleado *vemp);
+/////////////////////////////////////////////////////////////////////////////
+
+void cargar_datos (int *ml, tvecempleados vemp);
+
+	void validar_fecha (int *dia, int *mes, int *anio);
+	void validar_sexo (char *sexo);
+	void obtener_nacionalidad (tvecnaciones vecnaciones);
+	void validar_datos (int *num, int max, int min);
+
+void menu (int ml, tvecempleados vemp, char apellido[]);
+
+	void opciones_Aejecutar (int ml, tvecempleados vemp, char apellido[], int opc);	
+	void ordenar_porNacimiento (int ml, tvecempleados vemp);
+	void mostrarNacidosAntesdeDosmil (int ml, tvecempleados vemp);
 	
-void ordenar_porNacimiento (int ml, tvecempleado vemp);
+	void mostrarUruyArgconmasdeunaNacionalidad (int ml, tvecempleados vemp);
+		int buscar_otraNacionalidad (int max_naciones, tvecnaciones vecnaciones);
 	
-void mostrarNacidosAntesdeDosmil (int ml, tvecempleado vemp);
-	
-void buscaryListarMujeresMismoApellido (int ml, tvecempleado vemp);
-	
-void mostrarUruyArgconmasdeunaNacionalidad (int ml, tvecempleado vemp);
-	
-int porcArgentinos (int ml, tvecempleado vemp);
+	void BuscaryMostrar_MujerVeranomismoApellido (int ml, tvecempleados vemp, char apellido []);
+		bool mujer_nacidaVerano (char sexo, int mes);
+		
+	float porcArgentinos (int ml, tvecempleados vemp);
 
-void mostrar_datos (int ml, tvecempleado vemp);
+void mostrar_datos (int numero, char nombres[MAX], int dia, int mes, int anio, char sexo, tvecnaciones vecnaciones);
 
-void validar_datos (int *num, int max, int min);
-
-void validar_fecha (int *dia, int *mes, int *anio);
-
-void validar_sexo (char *sexo);
-
-void obtener_nacionalidad (tvecnaciones *vecnaciones);
+/////////////////////////////////////////////////////////////////////////////
 
 void main (){
 	
 	int ml;
-	tvecempleado vemp;
+	char apellido[MAX];
+	tvecempleados vemp;
 	
-	cargar_datos (&ml, &vemp);
+	cargar_datos (&ml, vemp);
+	printf("\n");
+	system("pause");
 	
-	ordenar_porNacimiento (ml, vemp);//falta
-	
-	mostrarNacidosAntesdeDosmil (ml, vemp);
-	
-	buscaryListarMujeresMismoApellido (ml, vemp);
-	
-	mostrarUruyArgconmasdeunaNacionalidad (ml, vemp);
-	
-	printf ("\n|-Porcentaje de empleados Argentinos: %f",porcArgentinos (ml, vemp));
+	menu (ml, vemp, apellido);
 	
 }
 
-void cargar_datos (int *ml, tvecempleado *vemp){
+void menu (int ml, tvecempleados vemp, char apellido[]){
+	
+	int opcion;
+	printf ("\n\t\t\t|----MENU----|\n");
+	
+	printf ("1. Mostrar nacidos antes del 2000 ordenado por nacimiento.\n");
+	printf ("2. Buscar y mostrar datos de apellido de mujer ingresado por usuario, nacidas en verano.\n");
+	printf ("3. Mostrar Uruguayos y Argentinos con mas de 1 nacionalidad.\n");
+	printf ("4. Mostrar porcentaje de empleados Argentinos.\n");
+	printf ("0. salir.\n");
+	printf ("\n|-seleccione la opcion a realizar o 0 para salir: ");
+	
+	scanf ("%i", &opcion);
+	printf ("\n\tValidando opcion...\n");
+	validar_datos (&opcion, 4, 0);
+	
+	while (opcion != 0){
+		
+		opciones_Aejecutar (ml, vemp, apellido, opcion);
+		
+		printf ("\n\t\t\t|----MENU----|\n");
+	
+		printf ("1. Mostrar nacidos antes del 2000 ordenado por nacimiento.\n");
+		printf ("2. Buscar y mostrar datos de apellido de mujer ingresado por usuario, nacidas en verano.\n");
+		printf ("3. Mostrar Uruguayos y Argentinos con mas de 1 nacionalidad.\n");
+		printf ("4. Mostrar porcentaje de empleados Argentinos.\n");
+		printf ("0. salir.\n");
+		printf ("\n|-seleccione la opcion a realizar o 0 para salir: ");
+		
+		scanf ("%i", &opcion);
+		printf ("\n\tValidando anio...\n");	
+		validar_datos (&opcion, 4, 0);
+	}
+	if (opcion == 0)
+		
+			printf ("\n\t\tAdios!!...\n");
+		
+	system ("pause");
+}
+void opciones_Aejecutar (int ml, tvecempleados vemp, char apellido[], int opc){
+	
+	switch (opc) {
+		case 1:
+		
+			ordenar_porNacimiento (ml, vemp);//falta
+			printf ("\n");
+		
+			mostrarNacidosAntesdeDosmil (ml, vemp);	
+			system("pause");
+				
+			break;
+		case 2:
+			
+			printf ("\n|-Ingrese apellido a buscar: ");
+			fflush (stdin);	
+			fgets (apellido, MAX, stdin);
+	
+			BuscaryMostrar_MujerVeranomismoApellido (ml, vemp, apellido);
+			system("pause");
+		
+			break;
+		case 3:
+			
+			mostrarUruyArgconmasdeunaNacionalidad (ml, vemp);
+			system("pause");
+				
+			break;
+		case 4:
+					
+			printf ("\n|-Porcentaje de empleados Argentinos: %f", porcArgentinos(ml, vemp));
+			printf ("\n");
+			break;
+	}
+}
+
+void cargar_datos (int *ml, tvecempleados vemp){
 	
 	int i;
 	//char aux;
@@ -77,36 +154,37 @@ void cargar_datos (int *ml, tvecempleado *vemp){
 	
 	do{
 		
-		printf ("|-Ingrese apellidos y nombres (apellido, nombre) o 'f' para finalizar: ");
-		fflush(stdin);
-		fgets (vemp[i]->apeynombre, MAX, stdin);
+		printf ("|-Ingrese apellidos y nombres (apellido,+ nombre) o 'f' para finalizar: ");
 		
-		if (*vemp[i]->apeynombre != 'f'){
+		fflush(stdin);
+		
+		fgets (vemp[i].apeynombre, MAX, stdin);
+		
+		if (*vemp[i].apeynombre != 'f'){
 			
 			printf ("\n|-Ingrese fecha de nacimiento (dd-mm-aa): ");
-			scanf ("%i %i %i", &vemp[i]->nacimiento.dia, &vemp[i]->nacimiento.mes, &vemp[i]->nacimiento.anio);
+			scanf ("%i %i %i", &vemp[i].nacimiento.dia, &vemp[i].nacimiento.mes, &vemp[i].nacimiento.anio);
 			
 			printf ("\n|-Validando fecha...\n");
-			validar_fecha (&vemp[i]->nacimiento.dia, &vemp[i]->nacimiento.mes, &vemp[i]->nacimiento.anio);
-			
+			validar_fecha (&vemp[i].nacimiento.dia, &vemp[i].nacimiento.mes, &vemp[i].nacimiento.anio);
+			printf ("\n");	
 			system("pause");
 			
 			printf ("\n|-Ingrese sexo (M/F): ");
-			scanf ("%c\n", &vemp[i]->sexo);
-			
+			fflush (stdin);
+			scanf("%c", &vemp[i].sexo);
 			
 			printf ("\n|-Validando sexo...\n");
-			validar_sexo (&vemp[i]->sexo);
+			validar_sexo (&vemp[i].sexo);
 			
 			system ("pause");
 			
-			obtener_nacionalidad(&vemp[i]->vecnaciones);
+			obtener_nacionalidad(vemp[i].vecnaciones);
 			
+			i++;
 		}
 		
-		i++;
-		
-	}while ((*vemp[i]->apeynombre != 'f') && (i != MF));
+	}while ((*vemp[i].apeynombre != 'f') && (i != MF));
 	
 	*ml = i;
 }
@@ -165,27 +243,20 @@ void validar_fecha (int *dia, int *mes, int *anio){
 
 void validar_sexo (char *sexo){
 
-	while (((*sexo != 'M') && (*sexo != 'F')) && ((*sexo != 'm') && (*sexo != 'f'))){//problemas con esta expresion logica
+	bool validado = true;
+
+	while (!validado){
 		
-		printf ("\n|-Error..");
-		printf ("\n|-Reingrese sexo(M/F): ");
-		fflush (stdin);
-		scanf ("%c", sexo);
-	}
-	
-	printf ("\n->sexo validado...\n");
-	
-	
-}
-	/*bool validado = false;
-	while (validado){
 		switch (*sexo){
+		
 			case 'M':
 			case 'm':
 			case 'F':
 			case 'f':
+				
 				validado = true;
 				break;
+				
 			default :
 				
 				printf ("\n|-Error..");
@@ -195,38 +266,29 @@ void validar_sexo (char *sexo){
 				break;
 		}
 	}
-	if (validado == true) {
-		
-		printf ("\n->sexo validado...\n");
-	}*/
+			
+	printf ("\n\t->sexo validado...\n");
+	printf("\n");
+}
 	
-/*	while (((*sexo != 'M') || (*sexo != 'm')) || ((*sexo != 'F') || (*sexo != 'f'))){
-		
-		printf ("\n|-Error..");
-		printf ("\n|-Reingrese sexo(M/F): ");
-		fflush (stdin);
-		scanf ("%c", sexo);
-	}
 	
-	printf ("\n->sexo validado...\n");*/
-	
-void obtener_nacionalidad(tvecnaciones *vecnaciones){
+void obtener_nacionalidad(tvecnaciones vecnaciones){
 	
 	int opc,i;
 	
-	for (i = 0; i <=brasilenio; i++)
+	for (i = 0; i < MAxNAC; i++)
 	{
-			*vecnaciones[i] = 0;
+			vecnaciones[i] = 0;
 	}
 	
 	printf ("\n|-Nacionalidades: ");
-	printf ("\n1. Argentino");
-	printf ("\n2. Uruguayo");
-	printf ("\n3. Chileno");
-	printf ("\n4. Peruano");
-	printf ("\n5. Boliviano");
-	printf ("\n6. Paraguayo");
-	printf ("\n7. Brasilenio");
+	printf ("\n1. Argentina");
+	printf ("\n2. Uruguaya");
+	printf ("\n3. Chilena");
+	printf ("\n4. Peruana");
+	printf ("\n5. Boliviana");
+	printf ("\n6. Paraguaya");
+	printf ("\n7. Brasilenia");
 	printf ("\n0. salir");
 		
 	printf ("\n|-Elija las nacionalidades o 0 para salir: ");
@@ -234,137 +296,197 @@ void obtener_nacionalidad(tvecnaciones *vecnaciones){
 	scanf ("%i", &opc);
 	
 	printf ("\nValidando opcion...\n");
-	validar_datos (&opc, 7, 0);
+	validar_datos (&opc, MAxNAC, 0);
 	
 	while (opc != 0){
 		
-		*vecnaciones[opc] = 1;
+		vecnaciones[opc] = 1;
 //		cant_nac = cant_nac + 1;		
 		printf ("\n|-Elija otra nacionalidad o 0 para salir: ");
 		fflush (stdin);
 		scanf ("%i", &opc);
-		validar_datos (&opc, 7, 0);	
-	}	
+		validar_datos (&opc, MAxNAC, 0);	
+	}
+	printf ("\n");	
 }
 
-void mostrarNacidosAntesdeDosmil (int ml, tvecempleado vemp){
+void mostrarNacidosAntesdeDosmil (int ml, tvecempleados vemp){
 	
 	int i;
 	
 	printf ("\n|-Listado de nacidos antes de 2000: ");
 	
-	for (i = 0; i<= ml; i++)
+	printf ("\n|-numero\t Apellidos y Nombres\t fecha de nacimiento\t  sexo\t  nacionalidades\n");
 	
+	for (i = 0; i< ml; i++){
 		if (vemp[i].nacimiento.anio < 2000)
 				
 			//	printf ("\n|-%i. %s ", i, vemp[i].apeynombre);
-				mostrar_datos (ml, vemp);
-	
+			mostrar_datos (i, vemp[i].apeynombre, vemp[i].nacimiento.dia, vemp[i].nacimiento.mes, vemp[i].nacimiento.anio, vemp[i].sexo, vemp[i].vecnaciones);
+	}
+	printf ("\n");		
 }
 
-void mostrar_datos (int ml, tvecempleado vemp){
+void mostrar_datos (int numero, char nombres[MAX], int dia, int mes, int anio, char sexo, tvecnaciones vecnaciones){
 	
-	int i,j;
-	
-	printf ("\n|-numero\t -Apellidos y Nombres\t -  fecha de nacimiento\t - sexo\t - nacionalidades");
-	
-	for (i = 0; i <= ml; i++){
+	int j;
 		
-		printf ("\n|-%i.\t -%s\t -%i %i %i\t -%c\t\t", (i+1), vemp[i].apeynombre, vemp[i].nacimiento.dia, vemp[i].nacimiento.mes, vemp[i].nacimiento.anio, vemp[i].sexo);
-				
-		for (j = argentino; j <=brasilenio; i++){
-		
-			if (vemp[i].vecnaciones[j] == 1){
+	printf ("|-%i.", (numero+1));
+	printf ("\t%s\t", nombres);
+	printf ("-%i", dia);
+	printf ("-%i", mes);
+	printf ("-%i\t", anio);
+	printf ("%c\t", sexo);
+	
+	for (j = argentina; j <=brasilenia; j++){
+
+		if (vecnaciones[j] == 1){
+	
+			switch (j){
 			
-				switch (j){
-			
-					case argentino:
-								printf ("Argentino\t");
-								break;
-					case uruguayo:
-								printf ("Uruguayo\t");
-								break;
-					case chileno:
-								printf ("Chileno\t");
-								break;
-					case peruano:
-								printf ("Peruano\t");
-								break;
-					case boliviano:
-								printf ("Boliviano\t");
-								break;
-					case paraguayo:
-								printf ("Paraguayo\t");
-								break;
-					case brasilenio:
-								printf ("Brasilenio\t");
-								break;
-				}	
+				case argentina:
+							printf ("  Argentina");
+							break;
+				case uruguaya:
+							printf ("  Uruguaya");
+							break;
+				case chilena:
+							printf ("  Chilena");
+							break;
+				case peruana:
+							printf ("  Peruana");
+							break;
+				case boliviana:
+							printf ("  Boliviana");
+							break;
+				case paraguaya:
+							printf ("  Paraguaya");
+							break;
+				case brasilenia:
+							printf ("  Brasilenia");
+							break;
 			}
+			printf ("\n");	
 		}
 	}
 }
 
-int porcArgentinos (int ml, tvecempleado vemp){
+float porcArgentinos (int ml, tvecempleados vemp){
 	
 	int i, cant_Arg;
 	
 	cant_Arg = 0;
 	
-	for ( i = 0; i <= ml; i++){
+	for ( i = 0; i < ml; i++){
 		
-		if ( vemp[i].vecnaciones[argentino] == 1)
+		if (vemp[i].vecnaciones[argentina] == 1)//me toma solo argentina, si elijo 2 nacionalidades ej arg y urug y urug y chil no me toma el porcentaje de el primero arg.
 			
-				cant_Arg ++;	
+				cant_Arg++;	
 	}
 	
 	return ((cant_Arg/ml)*100);
 }
 
-void buscaryListarMujeresMismoApellido (int ml, tvecempleado vemp){
+bool mujer_nacidaVerano (char sexo, int mes){
 	
-	char apellido[MAX];
 	
+	if (((sexo == 'f') || (sexo == 'F'))&&((mes >= 1) && (mes <= 3))){
+		
+		return true;	
+	
+	}else{
+		
+		return false;
+	}
+	
+}
+
+void BuscaryMostrar_MujerVeranomismoApellido (int ml, tvecempleados vemp, char apellido []){	
+
 	int i;
 	
 	char *posicion;
-	
-	printf ("\n|-Ingrese apellido a buscar: ");
-	fgets (apellido, MAX, stdin);
+	bool encontrada = false;
 	
 	printf ("\n|-Mujeres que se apellidan igual al ingresado y nacidas en verano: \n");
 	
+	printf ("\n|-numero\t Apellidos y Nombres\t fecha de nacimiento\t  sexo\t  nacionalidades\n");
 	for ( i = 0; i <= ml; i++){
 									
 		posicion = strstr(vemp[i].apeynombre, apellido);
 		
-		if ((posicion != NULL) && ((vemp[i].sexo == 'f') || (vemp[i].sexo == 'F')) && (vemp[i].nacimiento.mes >= 1) && (vemp[i].nacimiento.mes <= 3)){
+		if ((posicion != NULL) && ( mujer_nacidaVerano(vemp[i].sexo, vemp[i].nacimiento.mes) == true)){
 
-			mostrar_datos (ml, vemp);
+			mostrar_datos (i, vemp[i].apeynombre, vemp[i].nacimiento.dia, vemp[i].nacimiento.mes, vemp[i].nacimiento.anio, vemp[i].sexo, vemp[i].vecnaciones);
+			encontrada = true;
 		}
 	}
+	
+	if (encontrada == false)
+		
+		printf ("\n\t->No hay mujeres nacidas en verano que se apelliden igual al apellido buscado...\n");
+	
+	printf ("\n");
 }
 
-void mostrarUruyArgconmasdeunaNacionalidad (int ml, tvecempleado vemp){
-	
-	int i,j=argentino;
-	
-	for (i = 0; i <= ml; i++){
+int buscar_otraNacionalidad (int max_naciones, tvecnaciones vecnaciones){
+
+	int i=argentina;
+	bool encontrado = false;
+
+	while ((i  < max_naciones) && (encontrado==false)){
 		
-		while (( j <= brasilenio) && (( vemp[i].vecnaciones[uruguayo] == 1 ) || (vemp[i].vecnaciones[argentino] == 1))){//si son igual a 1 significa que es uruguayo o argentino
+		if ((i != argentina) && (vecnaciones[argentina] == 1) && ( vecnaciones[i] == 1)){
 		
-			if (((j != uruguayo) || (j != argentino)) && (vemp[i].vecnaciones[j] == 1)){
-				
-				mostrar_datos (ml, vemp);
-			}
-			
-			j++;
-		}	
+			encontrado = true;
+		}else{
+			if ((i != uruguaya) && (vecnaciones[uruguaya] == 1) && ( vecnaciones[i] == 1))
+				encontrado = true;
+		}
+		i++;
 	}
+	
+	if (encontrado == true){
+		return 0;	
+	}/*else{
+	
+		return 1;
+	}*/
 }
 
-void ordenar_porNacimiento (int ml, tvecempleado vemp){
+
+void mostrarUruyArgconmasdeunaNacionalidad (int ml, tvecempleados vemp){
+	
+	int i;
+	
+	bool mas_nac = false;
+	
+	printf ("\n|-argentinos y uruguayos con mas de 1 nacionalidad: \n");
+
+	printf ("\n|-numero\t Apellidos y Nombres\t fecha de nacimiento\t  sexo\t  nacionalidades\n");
+
+	for ( i = 0; i< ml; i++) {
+	
+		if (buscar_otraNacionalidad(MAxNAC, vemp[i].vecnaciones) == 0){
+				
+			mostrar_datos (i, vemp[i].apeynombre, vemp[i].nacimiento.dia, vemp[i].nacimiento.mes, vemp[i].nacimiento.anio, vemp[i].sexo, vemp[i].vecnaciones);
+
+			printf ("\n");
+
+			mas_nac = true;
+		}
+	}
+	
+	if (mas_nac == false)
+			
+			printf ("\n\t->no hay empleados Uruguayos ni Argentinos con mas de 1 nacionalidad...\n");
+	
+	printf ("\n");			
+}
+
+void ordenar_porNacimiento (int ml, tvecempleados vemp){
 
 	
+
 
 }
